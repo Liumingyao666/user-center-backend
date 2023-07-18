65f834f32b63832a19092fffe09811137d02f072
Liumingyao666 <1806859182@qq.com>
2023-07-18 22:36:23 +0800

    version3.0

---
+++ src/main/java/com/liumingyao/usercenter/mapper/TeamMapper.java
@@ -0,0 +1,18 @@
+package com.liumingyao.usercenter.mapper;
+
+import com.liumingyao.usercenter.model.entity.Team;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author LiuMingyao
+* @description 针对表【team(队伍)】的数据库操作Mapper
+* @createDate 2023-07-12 14:51:44
+* @Entity com.liumingyao.usercenter.model.entity.Team
+*/
+public interface TeamMapper extends BaseMapper<Team> {
+
+}
+
+
+
+
---
+++ src/main/java/com/liumingyao/usercenter/mapper/UserTeamMapper.java
@@ -0,0 +1,18 @@
+package com.liumingyao.usercenter.mapper;
+
+import com.liumingyao.usercenter.model.entity.UserTeam;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author LiuMingyao
+* @description 针对表【user_team(用户队伍关系)】的数据库操作Mapper
+* @createDate 2023-07-12 14:53:13
+* @Entity com.liumingyao.usercenter.model.entity.UserTeam
+*/
+public interface UserTeamMapper extends BaseMapper<UserTeam> {
+
+}
+
+
+
+
